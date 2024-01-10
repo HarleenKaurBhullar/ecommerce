120603341869e535cac0a022abbcecad9cef25ac
HarleenKaurBhullar <145742023+HarleenKaurBhullar@users.noreply.github.com>
2024-01-10 18:17:28 +0530

    Add files via upload
    practice of an e commerce website

---
+++ practice.js
@@ -0,0 +1,10 @@
+
+
+
+function goToForm(productname,prprice){
+    document.getElementById("prname")=productname;
+    document.getElementById("prpice")=prprice;
+    
+    window.location.href="projectimages/buy.html"
+}
+  
